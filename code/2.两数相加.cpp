9df7c32fa26ca71cb27bc6ae29ed2a8c3f1d0f93
Terra <544708735@qq.com>
2025-04-17 16:03:21 +0800

    2

--- code/2.两数相加.cpp
+++ code/2.两数相加.cpp
@@ -16,13 +16,27 @@
  * };
  */
 
- struct ListNode;
+struct ListNode;
 
- 
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
+        ListNode dummy; // 哨兵节点
+        ListNode* cur = &dummy;
+        int carry = 0; // 进位
+        while (l1 || l2 || carry) { // 有一个不是空节点，或者还有进位，就继续迭代
+            if (l1) {
+                carry += l1->val; // 节点值和进位加在一起
+                l1 = l1->next; // 下一个节点
+            }
+            if (l2) {
+                carry += l2->val; // 节点值和进位加在一起
+                l2 = l2->next; // 下一个节点
+            }  
+            cur = cur->next = new ListNode(carry % 10); // 每个节点保存一个数位
+            carry /= 10; // 新的进位
+        }
+        return dummy.next; // 哨兵节点的下一个节点就是头节点
     }
 };
 // @lc code=end
